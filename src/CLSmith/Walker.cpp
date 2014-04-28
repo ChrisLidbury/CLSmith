40e80a3d3ebc630aba18126809c597c66c1ae5b5
Chris Lidbury <cdl110@doc.ic.ac.uk>
2014-04-28 21:26:10 +0100

    Walker for traversing the AST, templates were a waste of time. Divergence coming soon.

---
+++ src/CLSmith/Walker.cpp
@@ -0,0 +1,245 @@
+#include "CLSmith/Walker.h"
+
+#include <memory>
+#include <stack>
+#include <vector>
+
+#include "CFGEdge.h"
+#include "FactMgr.h"
+#include "Function.h"
+#include "Statement.h"
+#include "StatementFor.h"
+#include "StatementGoto.h"
+#include "StatementIf.h"
+
+namespace CLSmith {
+namespace Walker {
+namespace Internal {
+
+BlockWalker *CreateBlockWalker(Block *block) {
+  return new BlockWalker(block);
+}
+
+BlockWalker *CreateBlockWalkerAtStatement(Block *block, Statement *statement) {
+  std::unique_ptr<BlockWalker> block_walker(new BlockWalker(block));
+  assert(block_walker->AdvanceToStatement(statement));
+  block_walker->AdvanceSelector(statement);
+  return block_walker.release();
+}
+
+bool WalkerBase::AdvanceOne() {
+  if (!statement_) block_it_ = block_->stms.begin();
+  if (block_it_ == block_->stms.end()) return false;
+  statement_ = *block_it_;
+  ++block_it_;
+  return true;
+}
+
+bool WalkerBase::AdvanceToStatement(Statement *statement) {
+  assert(statement != NULL);
+  if (!statement_) block_it_ = block_->stms.begin();
+  while (block_it_ != block_->stms.end() && *block_it_ != statement)
+    ++block_it_;
+  if (block_it_ == block_->stms.end()) return false;
+  statement_ = *block_it_;
+  ++block_it_;
+  return true;
+}
+
+// Specialisation for a block statement. Not sure if this makes sense...
+template <>
+bool WalkerImpl<eBlock>::Advance(Statement *statement) {
+  std::cout << "Block wut" << std::endl;
+  return true;
+}
+
+// Specialisation for advancing on a for statement.
+bool WalkerImpl<eFor>::Advance(Statement *statement) {
+  // I'll worry about const correctness later ;)
+  for_body_.reset(CreateBlockWalker(const_cast<Block *>(
+      dynamic_cast<StatementFor *>(statement)->get_body())));
+  return true;
+}
+
+// Specialisation for if-then-else statements. else-ifs are nested in the else
+// block.
+bool WalkerImpl<eIfElse>::Advance(Statement *statement) {
+  StatementIf *statement_if = dynamic_cast<StatementIf *>(statement);
+  assert(statement_if != NULL);
+  if_body_.reset(CreateBlockWalker(
+      const_cast<Block *>(statement_if->get_true_branch()))); // const lol
+  Block *else_block = const_cast<Block *>(statement_if->get_false_branch());
+  else_body_.reset(else_block != NULL ? CreateBlockWalker(else_block) : NULL);
+  return true;
+}
+
+// Specialisation for goto statements. A blockWalker will be created for the
+// destination of the goto.
+bool WalkerImpl<eGoto>::Advance(Statement *statement) {
+  std::cout << "Found goto" << std::endl;
+  destination_ = const_cast<Statement *>(
+      dynamic_cast<StatementGoto *>(statement)->dest); // const lol
+  // Find out if it is a forward edge.
+  FactMgr *fact_mgr = get_fact_mgr_for_func(statement->func);
+  const CFGEdge *cfg_edge = NULL;
+  for (unsigned i = 0; i < fact_mgr->cfg_edges.size(); ++i)
+    if (statement == fact_mgr->cfg_edges[i]->src) {
+      cfg_edge = fact_mgr->cfg_edges[i];
+      break;
+    }
+  assert(cfg_edge != NULL);
+  assert(cfg_edge->dest == destination_);
+  goto_destination_is_forward_ = !cfg_edge->back_link;
+  return true;
+}
+
+// No matter what I do, I cannot get rid of this.
+// I have tried funciton pointers, traits and even tags.
+bool BlockWalker::AdvanceSelector(Statement *statement) {
+  assert(statement != NULL);
+  switch (statement->get_type()) {
+    case eAssign:   return WalkerImpl<eAssign>::Advance(statement);
+    case eBlock:    return WalkerImpl<eBlock>::Advance(statement);
+    case eFor:      return WalkerImpl<eFor>::Advance(statement);
+    case eIfElse:   return WalkerImpl<eIfElse>::Advance(statement);
+    case eInvoke:   return WalkerImpl<eInvoke>::Advance(statement);
+    case eReturn:   return WalkerImpl<eReturn>::Advance(statement);
+    case eContinue: return WalkerImpl<eContinue>::Advance(statement);
+    case eBreak:    return WalkerImpl<eBreak>::Advance(statement);
+    case eGoto:     return WalkerImpl<eGoto>::Advance(statement);
+    case eArrayOp:  return WalkerImpl<eArrayOp>::Advance(statement);
+    default: assert(false && "Invalid statement.");
+  }
+}
+
+}  // namespace Internal
+
+using Internal::BlockWalker;
+using Internal::WalkerImpl;
+
+FunctionWalker *FunctionWalker::CreateFunctionWalkerAtStatement(
+    Function *function, Statement *statement) {
+  assert(function != NULL);
+  assert(statement != NULL);
+
+  // Iteratively create the block walkers from the most nested block outward.
+  std::stack<BlockWalker *> nested_blocks_rev;
+  Block *block = statement->parent;
+  assert(block != NULL);
+  while (block != NULL) {
+    BlockWalker *block_walker = Internal::CreateBlockWalkerAtStatement(
+        block, statement);
+    // Check which branch the previous block was in, and remove it from the
+    // parents BlockWalker. Otherwise, we would reenter it when we advance.
+    if (!nested_blocks_rev.empty()) {
+      BlockWalker *branch = nested_blocks_rev.top();
+      if (statement->get_type() == eFor) {
+        branch->WalkerImpl<eFor>::for_body_.reset();
+      } else if (statement->get_type() == eIfElse) {
+        branch->WalkerImpl<eIfElse>::if_body_.reset();
+        if (block == dynamic_cast<StatementIf *>(statement)->get_false_branch())
+          branch->WalkerImpl<eIfElse>::else_body_.reset();
+      }
+    }
+    // Setup for next iteration.
+    block = block->parent;
+    // TODO Does this work?? lol const
+    statement = const_cast<Statement *>(statement->find_container_stm());
+    nested_blocks_rev.push(block_walker);
+  }
+
+  // The stack we have created is in the reverse order.
+  std::unique_ptr<FunctionWalker> function_walker(new FunctionWalker());
+  function_walker->function_ = function;
+  while (!nested_blocks_rev.empty()) {
+    function_walker->nested_blocks_.emplace(nested_blocks_rev.top());
+    nested_blocks_rev.pop();
+  }
+  function_walker->block_walker_.reset(
+      function_walker->nested_blocks_.top().release());
+  function_walker->nested_blocks_.pop();
+  return function_walker.release();
+}
+
+// This probably needs cleaning.
+bool FunctionWalker::Advance() {
+  blocks_exited_ = 0;
+  blocks_entered_ = 0;
+
+  // Check if we must enter a branch before advancing. (If newly constructed,
+  // must first Advance).
+  eStatementType type;
+  if (GetCurrentStatement() == NULL) goto derp;
+  type = GetCurrentStatementType();
+  if (type == eFor) {
+    BlockWalker *block_walker =
+        block_walker_->WalkerImpl<eFor>::for_body_.release();
+    if (block_walker != NULL) {
+      EnterBranch(block_walker);
+      ++blocks_entered_;
+    }
+  } else if (type == eIfElse) {
+    BlockWalker *block_walker =
+        block_walker_->WalkerImpl<eIfElse>::if_body_.release();
+    if (block_walker != NULL) {
+      EnterBranch(block_walker);
+      ++blocks_entered_;
+    }
+  }
+ derp:
+
+  if (block_walker_->AdvanceBlock()) return true;
+
+  // Reach the end of the current block. If there are any blocks on the stack
+  // (i.e. we are in a branch), pop one off and continue.
+  do {
+    if (nested_blocks_.empty()) return false;
+    block_walker_.reset(nested_blocks_.top().release()); // = seems to not work.
+    nested_blocks_.pop();
+    ++blocks_exited_;
+    // If we are exiting a branch, and the statement has multiple branches.
+    if (MustEnterBranch()) break;
+    // The stuff after the do loop is specifically for multiple branches, so we
+    // must return early.
+    if (block_walker_->AdvanceBlock()) return true;
+  } while (true);
+
+  // Should only get here if we have exited a branch.
+  type = GetCurrentStatementType();
+  if (type == eFor)
+    // We should have already entered the for body at this point. (Dead code)
+    assert(block_walker_->WalkerImpl<eFor>::for_body_.get() == NULL);
+  if (type == eIfElse) {
+    // We should have already entered the true branch at this point.
+    assert(block_walker_->WalkerImpl<eIfElse>::if_body_.get() == NULL);
+    BlockWalker *block_walker =
+        block_walker_->WalkerImpl<eIfElse>::else_body_.release();
+    if (block_walker != NULL) {
+      EnterBranch(block_walker);
+      ++blocks_entered_;
+    }
+    // Advance if we enter the else block. As long as there are no empty blocks,
+    // we should not have to do anything else.
+    assert(block_walker_->AdvanceBlock());
+  }
+
+  return true;
+}
+
+bool FunctionWalker::MustEnterBranch() {
+  eStatementType type = GetCurrentStatementType();
+  if (type == eFor)
+    return block_walker_->WalkerImpl<eFor>::for_body_ != NULL;
+  if (type == eIfElse)
+    return block_walker_->WalkerImpl<eIfElse>::if_body_ ||
+           block_walker_->WalkerImpl<eIfElse>::else_body_;
+  return false;
+}
+
+void FunctionWalker::EnterBranch(BlockWalker *block_walker) {
+  nested_blocks_.push(std::move(block_walker_));
+  block_walker_.reset(block_walker);
+}
+
+}  // namespace Walker
+}  // namespace CLSmith
---
+++ src/CLSmith/Walker.h
@@ -0,0 +1,248 @@
+// Generic walker for traversing the statements of a function.
+// Does not do any processing on the statements, it will simply make them
+// visible to other functions.
+//
+// This was all pretty much a waste of time!
+// I thought the templates would help >:(
+//
+// Future possible improvements:
+// - Get rid of the templated stuff.
+// - const correctness (if nothing needs to modify the block) and change
+//   pointers to refs.
+// - Use CRTP to pass the derived class down the hierarchy, might allow us to
+//   get rid of the AdvanceSelector.
+// - FunctionWalker accesses the BlockWalker's stuff manually. Add some accessor
+//   functions to BlockWalker and use those instead.
+
+#ifndef _CLSMITH_WALKER_H_
+#define _CLSMITH_WALKER_H_
+
+#include <iostream>
+#include <memory>
+#include <stack>
+#include <vector>
+
+#include "Block.h"
+#include "CommonMacros.h"
+#include "Function.h"
+#include "Statement.h"
+
+namespace CLSmith {
+namespace Walker {
+namespace Internal {
+
+class BlockWalker;
+
+// Simple factory method for creating a BlockWalker at the start of a block.
+// Allows the BlockWalkers to recursively create other BlockWalkers.
+BlockWalker *CreateBlockWalker(Block *block);
+
+// Create a BlockWalker at a specified position in the block.
+BlockWalker *CreateBlockWalkerAtStatement(Block *block, Statement *statement);
+
+// Generic type list, simply holds a compile time list of types.
+template <eStatementType... types>
+struct TypeList;
+
+// All of the statement types that will be traversed by the Walker.
+typedef TypeList<eAssign, eBlock, eFor, eIfElse,
+                 eInvoke, eReturn, eContinue, eBreak,
+                 eGoto, eArrayOp> StatementList;
+
+// Base of the Walker hierarchy. Everything that the Walker depends on should be
+// here.
+class WalkerBase {
+ public:
+  WalkerBase() : block_(NULL), statement_(NULL) {}
+  virtual ~WalkerBase() {}
+  bool AdvanceOne();
+  bool AdvanceToStatement(Statement *statement);
+  Block *block_;
+  Statement *statement_;
+ private:
+  std::vector<Statement *>::iterator block_it_;
+};
+
+// Basic declaration, templated with our list of statements.
+template <class TypeList>
+class Walker;
+
+// Basic implementation declaration, templated with a statement.
+template <eStatementType Type>
+class WalkerImpl;
+
+// End of the recursive Walker hierarchy, represented by the empty TypeList.
+template <>
+class Walker<TypeList<>> : public virtual WalkerBase {
+};
+
+// Recursive structure for the walker.
+// Each type in our statement list will appear in 'StatementType', and so
+// everything in this class will be defined for each statement.
+// This class will also be extended with a specialisation of the WalkerImpl
+// class for each statement.
+template <eStatementType Type, eStatementType... Types>
+class Walker<TypeList<Type, Types...>>
+    : public WalkerImpl<Type>,
+      public Walker<TypeList<Types...>> {
+};
+
+// Specialisation of WalkerImpl for For statements, necessary to keep track of
+// the Walker for the body block.
+template <>
+class WalkerImpl<eFor> : public virtual WalkerBase {
+ public:
+  bool Advance(Statement *statement);
+  std::unique_ptr<BlockWalker> for_body_;
+};
+
+// Specialisation for the if-then-else implementation, must keep track of all
+// the branches that may occur, there is no limit.
+template <>
+class WalkerImpl<eIfElse> : public virtual WalkerBase {
+ public:
+  bool Advance(Statement *statement);
+  std::unique_ptr<BlockWalker> if_body_;
+  std::unique_ptr<BlockWalker> else_body_;
+};
+
+// Goto implementation. A BlockWalker for the destination is not created, as
+// unless the source and destination is in the same block, it will not help us;
+// a FunctionWalker would be better, allowing us to walk between the two points.
+template <>
+class WalkerImpl<eGoto> : public virtual WalkerBase {
+ public:
+  bool Advance(Statement *statement);
+  // Is the destination of the goto ahead of the statement.
+  Statement *destination_;
+  bool goto_destination_is_forward_;
+};
+
+// Generic template for the Walker of a given statement. A specialisation is
+// only needed if yopu need extra members for the statement.
+template <eStatementType S>
+class WalkerImpl : public virtual WalkerBase {
+ public:
+  bool Advance(Statement *statement);
+};
+
+// Forward declare block specialisation. Needed as there is no block
+// specialisation of WalkerImpl.
+template <>
+bool WalkerImpl<eBlock>::Advance(Statement *statement);
+
+// Generic template for processing a basic statement. Advance is a misnomer
+// here, we always advance on every statement, and then apply the Advance method
+// in WalkerImpl to process what we found.
+template <eStatementType S>
+bool WalkerImpl<S>::Advance(Statement *statement) {
+  return true;
+}
+
+// Simple Walker that brings the implementations together.
+// This is specifically for walking through a block, branches do not sit in the
+// same block, but form their own. The implementations provide functionality for
+// handling this, but entering the branches must be done manually.
+class BlockWalker : public Walker<StatementList> {
+ public:
+  BlockWalker() {}
+  explicit BlockWalker(Block *block) { WalkerBase::block_ = block; }
+  bool AdvanceBlock() {
+    return WalkerBase::AdvanceOne() && AdvanceSelector(WalkerBase::statement_);
+  }
+  bool AdvanceSelector(Statement *statement);
+};
+
+}  // namespace Internal
+
+// Traverses the blocks in a function. Each branch in the function will start a
+// separate block which must be kept track of.
+class FunctionWalker {
+ public:
+  explicit FunctionWalker(Function *function) : function_(function) {
+    assert(function_);
+    block_walker_.reset(Internal::CreateBlockWalker(function_->body));
+    
+  }
+  FunctionWalker(FunctionWalker&& other) = default;
+  FunctionWalker& operator=(FunctionWalker&& other) = default;
+  virtual ~FunctionWalker() {}
+
+  // Factory for creating a walker at a specific point in a function.
+  // The state of the walker will be the same as if the walker had advanced to
+  // the statement from the beginning.
+  static FunctionWalker *CreateFunctionWalkerAtStatement(
+      Function *function, Statement *statement);
+
+  // General function for advancing. Handles branching and gathering of all the
+  // necessary data.
+  virtual bool Advance();
+
+  // Information on the current statement/block.
+  Statement *GetCurrentStatement() const {
+    return block_walker_->WalkerBase::statement_;
+  }
+  eStatementType GetCurrentStatementType() const {
+    return block_walker_->WalkerBase::statement_->get_type();
+  }
+  Block *GetCurrentBlock() const {
+    return block_walker_->WalkerBase::block_;
+  }
+
+  // Gets a FunctionWalker for the destination of a Goto statement.
+  // Useful for walking between the goto and the destination:
+  //   std::unique_ptr<FunctionWalker> dest(src->GetGotoDestination());
+  //   if (!src->GotoDestinationIsForward())
+  //     while(dest != src) {
+  //       do stuff...
+  //       dest->Advance();
+  //     }
+  //
+  // Neither walker has any information on which branch the source/dest is in,
+  // and would need access to the nested blocks. A possible extension would be
+  // to extend this class with a 'GotoFunctionWalker' that could handle this.
+  FunctionWalker *GetGotoDestination() {
+    return CreateFunctionWalkerAtStatement(
+        function_, block_walker_->Internal::WalkerImpl<eGoto>::destination_);
+  }
+  bool GotoDestinationIsForward() {
+    return block_walker_->Internal::WalkerImpl<eGoto>::goto_destination_is_forward_;
+  }
+
+  // Equal if they are both at the same point in the same function.
+  bool operator==(const FunctionWalker& other) const {
+    return function_ == other.function_ &&
+        GetCurrentStatement() == other.GetCurrentStatement();
+  }
+  bool operator!=(const FunctionWalker& other) const {
+    return !(*this == other);
+  }
+
+  // Branching information.
+  int EnteredBranch() const { return blocks_entered_; }
+  int ExitedBranch() const { return blocks_exited_; }
+ protected:
+  // Simple helper function to inform us that there is a block (on a branch
+  // statement) that must be entered before we advance.
+  bool MustEnterBranch();
+  // block_walker must be a raw, unmanaged pointer.
+  void EnterBranch(Internal::BlockWalker *block_walker);
+
+  Function *function_;
+  std::unique_ptr<Internal::BlockWalker> block_walker_;
+  std::stack<std::unique_ptr<Internal::BlockWalker>> nested_blocks_;
+  // Represents how many levels of blocks that were changed on the last advance.
+  // Blocks entered should be no more than 1, and if it is 1, then the block is
+  // entered after the exited blocks. This assumes that there are no empty
+  // blocks.
+  int blocks_entered_;
+  int blocks_exited_;
+ private:
+  FunctionWalker() {}
+  DISALLOW_COPY_AND_ASSIGN(FunctionWalker);
+};
+
+}  // namespace Walker
+}  // namespace CLSmith
+
+#endif  // _CLSMITH_WALKER_H_
